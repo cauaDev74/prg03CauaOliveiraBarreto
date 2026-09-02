c96be1d0877d26f62fb3c39cdfbeb8ebd353b87e
cauaDev74 <barretocaua74@gmail.com>
2026-09-01 23:16:33 -0300

    feat: Atividade Tela Login

---
+++ Atividades/Atividade 04 - Tela Login/prg03CauaOliveiraBarreto/src/main/java/br/com/ifba/prg03cauaoliveirabarreto/Prg03CauaOliveiraBarreto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.prg03cauaoliveirabarreto;
+
+/**
+ *
+ * @author Admin
+ */
+public class Prg03CauaOliveiraBarreto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
